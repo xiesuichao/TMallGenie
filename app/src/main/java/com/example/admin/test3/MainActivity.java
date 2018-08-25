1a3b1c1d60b86d039ae8b93a8e1b5a32c6c9df21
xiesuichao <xiesuichao@wxbtc.com>
2018-08-25 16:54:46 +0800

    仿天猫精灵滑动效果

---
+++ app/src/main/java/com/example/admin/test3/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.admin.test3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CustomLinearLayout mContainerCll;
+    private CustomScrollView mMainSv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+
+    }
+
+    private void initView(){
+
+        mMainSv = findViewById(R.id.sv_main);
+        mContainerCll = findViewById(R.id.cll_item_container);
+
+
+        mMainSv.setCustomLinearLayout(mContainerCll);
+        mContainerCll.setCustomScrollView(mMainSv);
+
+    }
+
+
+
+
+}
